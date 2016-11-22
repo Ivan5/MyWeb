afd85dbaf886948cb59a8230b9f776c48934ac9c
Ivan5 <converse_ivan@hotmail.com>
2016-11-21 18:32:23 -0600

    Added files and action to the quotes section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
-(function(){
+$(function(){
+	//Carousel
+	$('#owl-demo').owlCarousel({
+		navigation: false,
+		pagination: false,
+		slideSpeed: 300,
+		paginationSpeed: 400,
+		singleItem: true
+	});
 
-})();
+	
+});
